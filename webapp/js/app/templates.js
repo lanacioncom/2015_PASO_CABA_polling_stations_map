b203acf8fa81e5225d08cb5d35b13bb47d6e1d12
Juan Elosua <juan.elosua@gmail.com>
2015-04-29 15:48:42 -0300

    Remove unused vars

--- webapp/js/app/templates.js
+++ webapp/js/app/templates.js
@@ -1,8 +1,6 @@
 define(['text!templates/credits.html',
     'text!templates/overlay.html',
     'text!templates/popup.html',
-    'text!templates/sections.html',
-    'text!templates/sections_polling_tables.html',
     'text!templates/cartocss.css',
     'text!templates/feature_click.sql'], 
     function(credits, overlay, popup, sections, polling, cartocss, feature_click_sql) {
@@ -10,8 +8,6 @@ define(['text!templates/credits.html',
             credits: credits,
             overlay: overlay,
             popup: popup,
-            sections: sections,
-            polling: polling,
             cartocss: cartocss,
             feature_click_sql: feature_click_sql
         }
